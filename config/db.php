f6d2b0ae774c7b94f3f21402a2eac37f1f8b4675
abdullayevbahrom <fergusuz1313@gmail.com>
2026-02-16 20:48:26 +0500

    feat: add wf

--- config/db.php
+++ config/db.php
@@ -1,10 +1,16 @@
 <?php
 
+$host = getenv('DB_HOST') ?: 'localhost';
+$dbName = getenv('DB_NAME') ?: 'app';
+$port = getenv('DB_PORT') ?: 3306;
+$user = getenv('DB_USER') ?: 'root';
+$password = getenv('DB_PASSWORD') ?: 'password';
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=db;dbname=shop;port=3306',
-    'username' => 'shop',
-    'password' => 'password',
+    'dsn' => "mysql:host=$host;dbname=$dbName;port=$port",
+    'username' => $user,
+    'password' => $password,
     'charset' => 'utf8',
     'enableSchemaCache' => true,
     'schemaCacheDuration' => 60,
