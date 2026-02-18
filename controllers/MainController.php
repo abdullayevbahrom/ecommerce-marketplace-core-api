93955e2420c6812a5853581fd7f90725e677d626
abdullayevbahrom <fergusuz1313@gmail.com>
2026-02-18 15:11:40 +0500

    fix: logger

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -1,22 +1,33 @@
 <?php
+
 namespace app\controllers;
 
 use Yii;
 use yii\web\Controller;
 
-class MainController extends Controller {
-    public function actionIndex() {
+class MainController extends Controller
+{
+    public function actionIndex()
+    {
         return $this->redirect('/admin');
         return $this->render('index');
     }
 
-    public function actionLogOut() {
+    public function actionLogOut()
+    {
         Yii::$app->user->logout();
         return $this->goHome();
     }
 
-    public function actionError() {
+    public function actionError()
+    {
         $this->layout = 'error';
+        $exception = Yii::$app->errorHandler->exception;
+
+        if ($exception !== null) {
+            return $this->render('error', ['exception' => $exception]);
+        }
+        
         return $this->render('error');
     }
 }
