a73de03a76af8778bc5e8a34cdc47307fa4bd2a9
abdullayevbahrom <fergusuz1313@gmail.com>
2026-03-02 17:45:24 +0500

    fix: cors error

--- modules/api/controllers/SessionController.php
+++ modules/api/controllers/SessionController.php
@@ -32,17 +32,17 @@ class SessionController extends Controller
     public function behaviors()
     {
         $behaviors = parent::behaviors();
-    $behaviors['corsFilter'] = [
-        'class' => Cors::class,
-        'cors' => [
-            'Origin' => ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:3000'],
-            'Access-Control-Request-Method' => ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
-            'Access-Control-Request-Headers' => ['*'],
-            'Access-Control-Allow-Credentials' => true,
-            'Access-Control-Max-Age' => 86400,
-            'Access-Control-Expose-Headers' => ['X-Pagination-Total-Count','X-Pagination-Page-Count','X-Pagination-Current-Page','X-Pagination-Per-Page'],
-        ],
-    ];
+        $behaviors['corsFilter'] = [
+            'class' => Cors::class,
+            'cors' => [
+                'Origin' => ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:3000'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+                'Access-Control-Allow-Credentials' => true,
+                'Access-Control-Max-Age' => 86400,
+                'Access-Control-Expose-Headers' => ['X-Pagination-Total-Count', 'X-Pagination-Page-Count', 'X-Pagination-Current-Page', 'X-Pagination-Per-Page'],
+            ],
+        ];
 
         $behaviors['verbs'] = [
             'class' => VerbFilter::class,
