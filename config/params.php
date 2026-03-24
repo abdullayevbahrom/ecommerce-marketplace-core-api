546bec855f25b5409194e86bac75f93ad4ba6e9b
abdullayevbahrom <fergusuz1313@gmail.com>
2026-03-24 12:31:59 +0500

    Wire reference event flag into shop config

--- config/params.php
+++ config/params.php
@@ -53,6 +53,7 @@ $config = [
         'user' => $rabbitMqUser,
         'password' => $rabbitMqPassword,
         'vhost' => '/',
+        'enable_reference_events' => filter_var(getenv('RABBITMQ_ENABLE_REFERENCE_EVENTS') ?: false, FILTER_VALIDATE_BOOL),
         'enable_moderation_events' => filter_var(getenv('RABBITMQ_ENABLE_MODERATION_EVENTS') ?: false, FILTER_VALIDATE_BOOL),
         'exchange_sklad_to_market' => $rabbitMqExchangeSkladToMarket,
         'exchange_market_to_sklad' => $rabbitMqExchangeMarketToSklad,
