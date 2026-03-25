dc39e87fa1d4e502a07135cae153b4fed4649fab
abdullayevbahrom <fergusuz1313@gmail.com>
2026-03-25 09:50:52 +0500

    Filter brand and shop facets by catalog results

--- modules/api/controllers/BrandController.php
+++ modules/api/controllers/BrandController.php
@@ -11,6 +11,8 @@ use yii\helpers\ArrayHelper;
 
 use app\models\Category;
 use app\models\brand\CategoryBrand;
+use app\models\product\Product;
+use app\models\product\ProductFilter;
 use yii\web\NotFoundHttpException;
 
 class BrandController extends Controller {
@@ -76,6 +78,21 @@ class BrandController extends Controller {
 
     public function actionIndex() {
         $query = CategoryBrand::find()->with('image', 'category')->where(['status'=>1]);
+        $context = $this->getCatalogFilterContext();
+
+        if ($this->hasFacetContext($context)) {
+            $brandIds = $this->buildFacetProductQuery($context, true)
+                ->select('product.brand_id')
+                ->andWhere(['not', ['product.brand_id' => null]])
+                ->distinct()
+                ->column();
+
+            if (empty($brandIds)) {
+                $query->andWhere(['id' => -1]);
+            } else {
+                $query->andWhere(['id' => $brandIds]);
+            }
+        }
 
         return new ActiveDataProvider([
             'query' => $query,
@@ -159,6 +176,138 @@ class BrandController extends Controller {
         $brand->save(false);
 
         return ['success' => true];
+    }
+
+    private function getCatalogFilterContext(): array
+    {
+        $params = Yii::$app->request->get();
+        $referer = Yii::$app->request->referrer;
+
+        if ($referer) {
+            $refererPath = parse_url($referer, PHP_URL_PATH) ?: '';
+            $refererQuery = parse_url($referer, PHP_URL_QUERY) ?: '';
+
+            if ($refererQuery !== '' && str_contains($refererPath, '/filter')) {
+                parse_str($refererQuery, $refererParams);
+                $params = array_replace_recursive($refererParams, $params);
+            }
+        }
+
+        return [
+            'query' => trim((string)($params['query'] ?? $params['q'] ?? '')),
+            'category_id' => isset($params['category_id']) ? (int)$params['category_id'] : null,
+            'brand_id' => isset($params['brand_id']) ? (int)$params['brand_id'] : null,
+            'shop_id' => isset($params['shop_id']) ? (int)$params['shop_id'] : null,
+            'price_min' => isset($params['price_min']) ? (float)$params['price_min'] : null,
+            'price_max' => isset($params['price_max']) ? (float)$params['price_max'] : null,
+            'filter' => is_array($params['filter'] ?? null) ? $params['filter'] : [],
+        ];
+    }
+
+    private function hasFacetContext(array $context): bool
+    {
+        return $context['query'] !== ''
+            || !empty($context['category_id'])
+            || !empty($context['brand_id'])
+            || !empty($context['shop_id'])
+            || $context['price_min'] !== null
+            || $context['price_max'] !== null
+            || !empty($context['filter']);
+    }
+
+    private function buildFacetProductQuery(array $context, bool $ignoreBrand = false)
+    {
+        $products = Product::find()
+            ->alias('product')
+            ->where(['product.status' => 1])
+            ->marketplaceVisible();
+
+        if ($context['query'] !== '') {
+            $searchFields = [
+                'product.name_ru',
+                'product.name_uz',
+                'product.name_en',
+                'product.name_trans_ru',
+                'product.name_trans_en',
+                'product.description_ru',
+                'product.description_uz',
+                'product.description_en',
+                'product.composition_ru',
+                'product.composition_uz',
+                'product.composition_en',
+                'product.recommendation_ru',
+                'product.recommendation_uz',
+                'product.recommendation_en',
+            ];
+
+            foreach (preg_split('/\s+/u', $context['query'], -1, PREG_SPLIT_NO_EMPTY) as $term) {
+                $termCondition = ['or'];
+                foreach ($searchFields as $field) {
+                    $termCondition[] = ['like', $field, $term];
+                }
+                $products->andWhere($termCondition);
+            }
+        }
+
+        if (!empty($context['category_id'])) {
+            $categoryIds = [(int)$context['category_id']];
+            $subcategories = Category::find()
+                ->select('id')
+                ->where(['parent_id' => (int)$context['category_id']])
+                ->column();
+
+            foreach ($subcategories as $subcatId) {
+                $categoryIds[] = (int)$subcatId;
+            }
+
+            $products->andWhere(['product.category_id' => array_values(array_unique($categoryIds))]);
+        }
+
+        if (!$ignoreBrand && !empty($context['brand_id'])) {
+            $products->andWhere(['product.brand_id' => (int)$context['brand_id']]);
+        }
+
+        if (!empty($context['shop_id'])) {
+            $products->andWhere(['product.shop_id' => (int)$context['shop_id']]);
+        }
+
+        if ($context['price_min'] !== null) {
+            $products->andWhere(['>=', 'product.price', $context['price_min']]);
+        }
+
+        if ($context['price_max'] !== null) {
+            $products->andWhere(['<=', 'product.price', $context['price_max']]);
+        }
+
+        if (!empty($context['filter'])) {
+            $productIds = [];
+            $filterCount = 0;
+
+            foreach ($context['filter'] as $filterId => $filterValue) {
+                $filterCount++;
+
+                $subQuery = ProductFilter::find()
+                    ->select('product_id')
+                    ->where(['filter_id' => $filterId])
+                    ->andWhere([
+                        'or',
+                        ['id' => $filterValue],
+                        ['value_ru' => $filterValue],
+                        ['value_en' => $filterValue],
+                        ['value_uz' => $filterValue],
+                    ]);
+
+                if ($filterCount === 1) {
+                    $productIds = $subQuery->column();
+                } else {
+                    $productIds = array_intersect($productIds, $subQuery->column());
+                }
+            }
+
+            $products->andWhere(['product.id' => empty($productIds) ? -1 : $productIds]);
+        }
+
+        return $products;
     }
 }
 ?>
