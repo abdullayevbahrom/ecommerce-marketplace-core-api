158375fefea4a83b1235f065828adfbaca67b522
abdullayevbahrom <fergusuz1313@gmail.com>
2026-03-25 16:08:38 +0500

    Redirect expired admin sessions to login

--- modules/admin/controllers/ShopDocumentController.php
+++ modules/admin/controllers/ShopDocumentController.php
@@ -27,6 +27,11 @@ class ShopDocumentController extends Controller
         }
         $this->user = User::find()->with('moderatorAccess', 'moderatorAccess.moderator')->where(['id'=>Yii::$app->user->identity->id])->one();
 
+        if (!$this->user) {
+            Yii::$app->user->logout(false);
+            return $this->redirect(["/admin/default"]);
+        }
+
         if (($this->user->role == User::ROLE_MODERATOR)) {
             $accesses = array();
 
