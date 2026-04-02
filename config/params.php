37bf7cdb873de6f3921b377c138b46d4e7fa2805
Bobur <derbibme@gmail.com>
2026-04-02 18:58:42 +0500

    fix: add skladApiUrl dev override for local docker networking

--- config/params.php
+++ config/params.php
@@ -132,6 +132,7 @@ $config = [
 if (YII_ENV_DEV) {
     $config['baseUrl'] = 'http://localhost:8001';
     $config['warehouseApiUrl'] = 'http://sklad';
+    $config['skladApiUrl'] = 'http://sklad';
     $config['operatorApiUrl'] = 'http://operator_app';  // TODO: update when operator is running
 }
 
